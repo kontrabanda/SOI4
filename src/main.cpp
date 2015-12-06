dab7d351a0db1f2564dd00b2d9f1108b90ce5bb8
Tomek <tomaszrydzewski92@gmail.com>
2015-12-06 22:03:36 +0100

    Dodanie drugiego producenta i konsumenta.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 
 Producer *producerA;
 Consumer *consumerA;
+Producer *producerB;
+Consumer *consumerB;
 Buffer *buffer;
 
 void produceA ();
@@ -19,12 +21,20 @@ int main () {
 	buffer = new Buffer();
 	producerA = new Producer(buffer, 'A', "ProducentA");
 	consumerA = new Consumer(buffer, "KonsumentA");
+	producerB = new Producer(buffer, 'B', "ProducentB");
+	consumerB = new Consumer(buffer, "KonsumentB");
 	
 	std::thread prodA(produceA);
 	std::thread consA(consumeA);
 
+	std::thread prodB(produceB);
+	std::thread consB(consumeB);
+
 	prodA.join();
 	consA.join();
+
+	prodB.join();
+	consB.join();
 
 	return 0;
 }
@@ -37,16 +47,22 @@ void produceA () {
 }
 
 void produceB () {
-
+	for (int i = 0; i < 100; ++i) {
+		producerB->produce();
+		usleep(300);
+	}
 }
 
 void consumeA () {
 	for (int i = 0; i < 100; ++i) {
 		consumerA->consume();
-		usleep(200);
+		usleep(400);
 	}
 }
 
 void consumeB () {
-
+	for (int i = 0; i < 100; ++i) {
+		consumerB->consume();
+		usleep(300);
+	}
 }
